f0f38843d6b1c942376c7ae2b14bfa073793d5f2
owenguo-UBC <owen.guowen@gmail.com>
2019-07-19 21:11:05 -0700

    basic email rest endpoint

---
+++ backend/src/rest/join/JoinController.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+
+JoinController = (req, res) => {
+    nodemailer.createTestAccount((err, account) => {
+        if (err) {
+            console.error('Failed to create a testing account. ' + err.message);
+            return process.exit(1);
+        }
+
+        // Create a SMTP transporter object
+        let transporter = nodemailer.createTransport({
+            host: account.smtp.host,
+            port: account.smtp.port,
+            secure: account.smtp.secure,
+            auth: {
+                user: account.user,
+                pass: account.pass
+            }
+        });
+
+        // Message object
+        let message = {
+            from: 'Sender Name <sender@example.com>',
+            to: 'Recipient <recipient@example.com>',
+            subject: 'Nodemailer is unicode friendly ✔',
+            text: 'Hello to myself!',
+            html: '<p><b>Hello</b> to myself!</p>'
+        };
+
+        transporter.sendMail(message, (err, info) => {
+            if (err) {
+                console.log('Error occurred. ' + err.message);
+                return process.exit(1);
+            }
+    
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            res.send("email sent!");
+        });
+    });
+
+    res.send("sending...");
+};
+
+module.exports = JoinController;
---
+++ deploy/install_docker.sh
@@ -0,0 +1,19 @@
+#Colors
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+sudo apt-get update
+
+#------------------DOCKER-CE INSTALL--------------------------------------------
+echo -e "${GREEN}==========Begining Docker Community Edition install==========${NC}"
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+#---------------DOCKER-COMPOSE INSTALL------------------------------------------
+echo -e "${GREEN}==========Begining Docker-Compose install===================${NC}"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
