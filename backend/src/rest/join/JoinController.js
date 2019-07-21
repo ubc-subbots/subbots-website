b2a6691108aa6e8b1dfdea9420f4ce810dded3d0
owenguo-UBC <owen.guowen@gmail.com>
2019-07-21 10:45:37 -0700

    added production email setup

--- backend/src/rest/join/JoinController.js
+++ backend/src/rest/join/JoinController.js
@@ -1,6 +1,69 @@
+const emailCreds = require(__dirname + '/../../secrets/email_cred.json');
 const nodemailer = require("nodemailer");
 
 JoinController = (req, res) => {
+    // sendEtheralEmail(req, res);  
+    sendProductionEmail(req, res);
+};
+
+function sendProductionEmail(req, res) {
+    if (!emailCreds) {
+        res.json({status: false, message: "ERROR: Failed to load credentials"});
+    }
+
+    const {
+        first_name,
+        last_name,
+        email,
+        year,
+        team,
+        reason
+    } = req.body;
+
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: emailCreds.user,
+            pass: emailCreds.pass
+        }
+    });
+
+    // Message object
+    let message = {
+        from: `${emailCreds.name} <${emailCreds.user}>`,
+        to: `${first_name} ${last_name} <${email}>`,
+        subject: 'Thanks for your interest!',
+        text: `Hi ${first_name} ${last_name}!\n\n(TODO: Create actual html email)`
+        // html: '<p><b>Hello</b> to myself!</p>'
+    };
+
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            res.json({
+                status: false, 
+                message: "ERROR: Failed to send email",
+                error: err.message
+            });
+        }
+
+        res.json({status: true, email_id: info.messageId});
+    });
+}
+
+//Used for testing
+function sendEtheralEmail(req, res) {
+    const {
+        first_name,
+        last_name,
+        email,
+        year,
+        team,
+        reason
+    } = req.body;
+
     nodemailer.createTestAccount((err, account) => {
         if (err) {
             console.error('Failed to create a testing account. ' + err.message);
@@ -20,11 +83,11 @@ JoinController = (req, res) => {
 
         // Message object
         let message = {
-            from: 'Sender Name <sender@example.com>',
-            to: 'Recipient <recipient@example.com>',
-            subject: 'Nodemailer is unicode friendly ✔',
-            text: 'Hello to myself!',
-            html: '<p><b>Hello</b> to myself!</p>'
+            from: 'UBC Subbots <sender@example.com>',
+            to: `${first_name} ${last_name} <${email}>`,
+            subject: 'Thanks for your interest!',
+            text: 'Woah'
+            // html: '<p><b>Hello</b> to myself!</p>'
         };
 
         transporter.sendMail(message, (err, info) => {
@@ -39,8 +102,6 @@ JoinController = (req, res) => {
             res.send("email sent!");
         });
     });
-
-    res.send("sending...");
-};
+}
 
 module.exports = JoinController;
