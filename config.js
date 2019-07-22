a100b384ba8ce70ec17b81c409aea7b13d7b13b5
owenguo-UBC <owen.guowen@gmail.com>
2019-07-21 20:27:23 -0700

    implemented production email controller

---
+++ config.js
@@ -0,0 +1,22 @@
+const config = {
+    "emails": {
+        "common_subbots_email": {
+          "name": "UBC Subbots",
+          "value": "info.subbots@gmail.com" //Temporary until production
+        },
+        "elec_team_lead_email": {
+            "name": "",
+            "value": ""
+        },
+        "mech_team_lead_email": {
+            "name": "",
+            "value": ""
+        },
+        "software_team_lead_email": {
+            "name": "",
+            "value": ""
+        }
+    }
+};
+
+module.exports = config;
