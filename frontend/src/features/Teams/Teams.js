d5a7be5aa4159343643f5bf2e8e22417f9e458eb
Logan Fillo <logan.fillo@gmail.com>
2019-08-06 18:04:04 -0600

    add team body and dockerfile

--- frontend/src/features/Teams/Teams.js
+++ frontend/src/features/Teams/Teams.js
@@ -4,29 +4,37 @@ import Col from 'react-bootstrap/Col';
 import Fade from 'react-reveal/Fade';
 import content from './content';
 import constants from '../../js/constants';
-import { TeamCard } from './Components';
+import { TeamCard, TeamBody } from './Components';
 import { ViewHeader, ViewContainer } from '../../components/View';
 import './styles.scss';
 
-export default class Teams extends React.Component{
+export default class Teams extends React.Component {
 
     render() {
         return (
             <ViewContainer>
-                <ViewHeader header={content.title} body={content.body}/>
+                <ViewHeader header={content.title} body={content.body} />
                 <Fade bottom duration={constants.FADE_DURATION}>
-                <Row>
-                    <Col sm={2}></Col>
-                    <Col sm={8}>
-                        <Row>
-                            <TeamCard team={'Software'}/>
-                            <TeamCard team={'Electrical'}/>
-                            <TeamCard team={'Mechanical'}/>
-                        </Row>
-                    </Col>
-                    <Col sm={2}></Col>
-                </Row>
+                    <Row>
+                        <Col sm={2}></Col>
+                        <Col sm={8}>
+                            <Row>
+                                <TeamCard team={'Software'} />
+                                <TeamCard team={'Electrical'} />
+                                <TeamCard team={'Mechanical'} />
+                            </Row>
+                        </Col>
+                        <Col sm={2}></Col>
+                    </Row>
                 </Fade>
+                <div className="SeperatorTop"></div>
+                <div className="TeamBodyContainer">
+                    <TeamBody team={'Software'} />
+                    <TeamBody team={'Electrical'} />
+                    <TeamBody team={'Mechanical'} />
+                </div>
+                <div className="SeperatorBottom"></div>
+
             </ViewContainer>
         )
     }
