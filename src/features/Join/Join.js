2866ab21333fb9b86a79df6e9ca88b5945cf88e7
owenguo-UBC <owen.guowen@gmail.com>
2019-08-25 16:58:21 -0700

    added formspree to bootstrap form

--- src/features/Join/Join.js
+++ src/features/Join/Join.js
@@ -9,6 +9,7 @@ import content from '../../content';
 import constants from '../../js/constants';
 import { ViewHeader, ViewContainer } from '../../components';
 import './styles.scss';
+import { continueStatement } from '@babel/types';
 
 export default class Join extends React.Component{
 
@@ -38,46 +39,52 @@ export default class Join extends React.Component{
 
     onReasonChange(event){this.setState({reason: event.target.value})}
 
-    onFormSubmit(event){
-        event.preventDefault();
-        let form = event.currentTarget;
-        if (form.checkValidity() === true){
-            let form_body = {
-                first_name: this.state.first_name,
-                last_name: this.state.last_name,
-                email: this.state.email,
-                year: this.state.year,
-                team: this.state.team,
-                reason: this.state.reason
-            }
-            axios.post('/rest/join', form_body)
-            .then( response => {
-                console.log(response);
-                const signee_res = response.data.signee_email.status;
-                const subbot_res = response.data.subbot_email.status;
+    // onFormSubmit(event){
+    //     event.preventDefault();
+    //     let form = event.currentTarget;
+    //     if (form.checkValidity() === true){
+    //         let form_body = {
+    //             first_name: this.state.first_name,
+    //             last_name: this.state.last_name,
+    //             email: this.state.email,
+    //             year: this.state.year,
+    //             team: this.state.team,
+    //             reason: this.state.reason
+    //         }
+    //         axios.post('/rest/join', form_body)
+    //         .then( response => {
+    //             console.log(response);
+    //             const signee_res = response.data.signee_email.status;
+    //             const subbot_res = response.data.subbot_email.status;
 
-                if (!signee_res && !subbot_res) {
-                    alert("ERROR: An unexpected email error has occured. Please try again in a few minutes");
-                } else if (!signee_res && subbot_res) {
-                    alert("INFO: Subbots has recieved your infomation, but your personal confirmation failed to send");
-                } else if (signee_res && !subbot_res) {
-                    //Case will never occur due to backend logic
-                } else {
-                    alert("Success! Your application has been sent");
-                }
-            })
-            .catch( error => {
-                console.log(error);
-            });
-        } else {
-            console.log("invalid form");
+    //             if (!signee_res && !subbot_res) {
+    //                 alert("ERROR: An unexpected email error has occured. Please try again in a few minutes");
+    //             } else if (!signee_res && subbot_res) {
+    //                 alert("INFO: Subbots has recieved your infomation, but your personal confirmation failed to send");
+    //             } else if (signee_res && !subbot_res) {
+    //                 //Case will never occur due to backend logic
+    //             } else {
+    //                 alert("Success! Your application has been sent");
+    //             }
+    //         })
+    //         .catch( error => {
+    //             console.log(error);
+    //         });
+    //     } else {
+    //         console.log("invalid form");
             
-        }
-        this.setState({validated: true});
+    //     }
+    //     this.setState({validated: true});
             
-    }
+    // }
 
     render() {
+        let teamLeadEmail = content.emails[this.state.team];
+
+        if (!teamLeadEmail) {
+            teamLeadEmail = content.emails.Subbots;
+        }
+
         return (
             <ViewContainer>
                 <ViewHeader title={content.join.title} blurb={content.join.blurb} image={content.join.image}/>
@@ -89,41 +96,46 @@ export default class Join extends React.Component{
                         <Card.Header className="JoinFormHeader">
                             UBC Subbots 2019-2020 Application
                         </Card.Header>
-                        <Form noValidate validated={this.state.validated} className="JoinForm" onSubmit={this.onFormSubmit.bind(this)}>
+                        <Form noValidate validated={this.state.validated} className="JoinForm" action={`https://formspree.io/${teamLeadEmail}`} method="POST">
+                            <input type="hidden" name="_subject" value="Someone has requested to join Subbots"/>
+                            {
+                                teamLeadEmail !== content.emails.Subbots &&
+                                <input type="hidden" name="_cc" value={content.emails.Subbots} />
+                            }
                             <Form.Row>
                                 <Form.Group as={Col}>
                                     <Form.Label>First Name *</Form.Label>
-                                    <Form.Control onChange={this.onFirstNameChange.bind(this)} placeholder="Enter first name" required />
+                                    <Form.Control onChange={this.onFirstNameChange.bind(this)} placeholder="Enter first name" required name="First Name" />
                                     <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Group as={Col}>
                                     <Form.Label>Last Name * </Form.Label>
-                                    <Form.Control onChange={this.onLastNameChange.bind(this)} placeholder="Enter last name" required/>
+                                    <Form.Control onChange={this.onLastNameChange.bind(this)} placeholder="Enter last name" required name="Last Name"/>
                                     <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>                                
                                 </Form.Group>
                             </Form.Row>
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>Email address * </Form.Label>
-                                <Form.Control onChange={this.onEmailChange.bind(this)} type="email" placeholder="Enter email" required/>
+                                <Form.Control onChange={this.onEmailChange.bind(this)} type="email" placeholder="Enter email" required name="Email"/>
                                 <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>                            
                             </Form.Group>
                             <Form.Row>
                                 <Form.Group as={Col}>
                                     <Form.Label>Year *</Form.Label>
-                                    <Form.Control onChange={this.onYearChange.bind(this)} as="select" required>
+                                    <Form.Control onChange={this.onYearChange.bind(this)} as="select" required name="Year">
                                         <option selected disabled hidden></option>
                                         <option>1</option>
                                         <option>2</option>
                                         <option>3</option>
                                         <option>4</option>
                                         <option>5</option>
-                                        <option>6</option>
+                                        <option>Other</option>
                                     </Form.Control>
                                     <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>                            
                                 </Form.Group>
                                 <Form.Group as={Col}>
                                     <Form.Label>Desired Team *</Form.Label>
-                                    <Form.Control onChange={this.onTeamChange.bind(this)} as="select" required>
+                                    <Form.Control onChange={this.onTeamChange.bind(this)} as="select" required name="Team">
                                         <option selected disabled hidden></option>
                                         <option>{constants.ELECTRICAL_TEAM}</option>
                                         <option>{constants.SOFTWARE_TEAM}</option>
@@ -134,7 +146,7 @@ export default class Join extends React.Component{
                             </Form.Row>
                             <Form.Group controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Reason for Application</Form.Label>
-                                <Form.Control onChange={this.onReasonChange.bind(this)} style={{resize:'none'}} as="textarea" rows="4" placeholder="Tell us why you want to join" />
+                                <Form.Control onChange={this.onReasonChange.bind(this)} style={{resize:'none'}} as="textarea" rows="4" placeholder="Tell us why you want to join" name="Application Reason"/>
                             </Form.Group>
                             <Button className="PrimaryButton" variant="warning" type="submit">
                                 <div className="ApplyButtonText" >Apply</div>
