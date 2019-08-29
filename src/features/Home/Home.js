aa36af0815fa7e5cfbf90087a8103d4f63ea3b48
owenguo-UBC <owen.guowen@gmail.com>
2019-08-28 20:53:13 -0700

    added sponsor images

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -13,7 +13,7 @@ export default class Home extends React.Component{
     render() {
         return (
             <ViewContainer>
-                <img className="logo" src={process.env.PUBLIC_URL + "/logo/vector_images/RGB(for_screen)/SVG/subbots-logo-rgb-svg-black-large.svg"}/>
+                <img className="logo" src={process.env.PUBLIC_URL + "/logo/vector_images/RGB(for_screen)/SVG/subbots-logo-rgb-yellow-large.svg"}/>
                 <ViewHeader title={content.home.title} blurb={content.home.blurb} disableImage={true}/>
                 <Row>
                     <Col sm={1}></Col>
