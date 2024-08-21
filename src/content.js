4038dba11d85ccd3898740a1b24062b81e246db9
Hashaam Zafar <hashaam1217@gmail.com>
2024-08-21 17:54:40 -0600

    Small Typo

--- src/content.js
+++ src/content.js
@@ -205,7 +205,7 @@ const content = {
     image: "join.jpg",
     title: "Join Our Team!",
     blurb:
-      "Our application cycle is open!. Be sure to apply on the link below!",
+      "Our application cycle is open! Be sure to apply on the link below!",
     link:
       "https://forms.gle/7Se9pCNASBqfKgP1A",
   },
